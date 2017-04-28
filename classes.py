43d34f20b7d3280d8ab078d37aa46393a8fc5c1e
akhatch2 <akhatch2@binghamton.edu>
2017-04-28 08:36:36 -0400

    Add files via upload

--- classes.py
+++ classes.py
@@ -2,9 +2,10 @@ import math
 import pygame
 class Grid:
 
-    def __init__(self, screen, startingplayer = 'C', initx = 7, inity = 6, initwidth = 50):
+    def __init__(self, screen, startingplayer = 'C', initx = 7, inity = 6, initwidth = 75):
         RED = (255, 0, 0)
-        self.color = RED
+        BLUE = (0, 0, 255)
+        self.color = BLUE
         self.matrix = [[0 for x in range(initx)]for x in range(inity)]
         self.initx = initx
         self.inity = inity
@@ -12,7 +13,7 @@ class Grid:
         self.screen = screen
         self.xOffset = 0
         self.yOffset = 75
-        self.boardYOffset = self.yOffset + 25
+        self.boardYOffset = self.yOffset + 40
         self.gridLineWidth = 1
         self.MovingPiecePosition = (self.initx//2)+1
         self.currentMovingPiece = 0
@@ -80,8 +81,12 @@ class Grid:
                         self.drawgrid()
                         print ("game over!!! no one won")
                     self.didPlayerWin()
+                    #self.canIwin()
+                        #if yes
+                            #self.ComputerMove()
+                        #else
+                            #self.canplayerwin()
 
-                    #self.ComputerMove()
                     #self.moveAndDrop()
                 else:
                     print("no move")
@@ -202,11 +207,12 @@ class Piece:
         GREEN = (0,255 , 0)
         BLUE = (0, 0, 255)
         RED = (255,0,0)
-        self.backColor = RED
+        YELLOW = (255,255,0)
+        self.backColor = BLUE
         if (playertype == "C"):
-            self.color = GREEN
+            self.color = YELLOW
         else:
-            self.color = BLUE
+            self.color = RED
         self.diameter = diameter -2
 
     def drawObject(self,screen,rect_x,rect_y):
@@ -224,10 +230,81 @@ class Piece:
     def RemoveMe(self,screen,rectY,rectX):
         pygame.draw.circle(screen, self.backColor, [rectX+self.diameter//2, rectY] ,((self.diameter-10)//2))
 
-    #def move(self, dire):
-    #    if(dire == 'a'):   #NTS restriction i.e. self.x-1 <= abs(3)
-    #        self.x - 1
-    #    elif(dire == 's'): #NTS goes down till there is a piece below or bottom of grid....while loop
-    #        self.y
-    #    elif(dire == 'd'): #NTS restriction i.e. self.x+1 <= abs(3)
-    #        self.x + 1
+class screens:
+    def __init__(self, fonttype, screen):
+        self.button1xloc = 125
+        self.button2xloc = 425
+        self.buttonyloc = 450
+        self.buttonlength = 100
+        self.buttonwidth = 50
+        self.fonttype = fonttype
+        self.screen = screen
+        self.labelxloc = 125
+        self.label1yloc = 200
+        self.label2yloc = 300
+        self.xmiddlelocation = (self.button1xloc + self.button2xloc) / 2
+
+    def text_objects(self, text, font, color):
+        textSurface = font.render(text, True, color)
+        return textSurface, textSurface.get_rect()
+
+    def screenmaking(self,label1, label2, screencolor, fontcolor, fontcolortxt, button1label, button2label, value):
+        RED = (255, 0, 0)
+        LIGHTRED = (100,0,0)
+        BLACK=(0,0,0)
+
+        end_it = False
+        while (end_it==False):
+            self.screen.fill(screencolor)
+            myfont = pygame.font.SysFont(self.fonttype, 40)
+            nlabel = myfont.render(label1, 1, fontcolor)
+            hlabel = myfont.render(label2, 2, fontcolor)
+            end_it = self.button(fontcolortxt, button1label, button2label, value)
+            for event in pygame.event.get():
+                if event.type==pygame.KEYDOWN:
+                    end_it=True
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            self.screen.blit(nlabel,(self.labelxloc,self.label1yloc))
+            self.screen.blit(hlabel,(self.labelxloc,self.label2yloc))
+            pygame.display.flip()
+
+    def button(self, fontcolortxt, button1label, button2label, value):
+        RED = (255, 0, 0)
+        LIGHTRED = (100,0,0)
+        BLACK=(0,0,0)
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if (value == 'no'):
+            if (self.xmiddlelocation + self.buttonlength > mouse[0] > self.xmiddlelocation and self.buttonyloc + self.buttonwidth > mouse[1] > self.buttonyloc):
+                pygame.draw.rect(self.screen, LIGHTRED, (self.xmiddlelocation, self.buttonyloc,self.buttonlength,self.buttonwidth))
+                if (click[0] == 1):
+                    return(True)
+            else:
+                pygame.draw.rect(self.screen, RED, (self.xmiddlelocation, self.buttonyloc,self.buttonlength,self.buttonwidth))
+            smallfont=pygame.font.SysFont(self.fonttype, 20)
+            textSurf, textRect = self.text_objects(button1label, smallfont, fontcolortxt)
+            textRect.center = ( ((self.xmiddlelocation)+(self.buttonlength/2))) , (self.buttonyloc+(self.buttonwidth/2))
+            self.screen.blit(textSurf, textRect)
+        if (value == 'yes'):
+            if (self.button1xloc + self.buttonlength > mouse[0] > self.button1xloc and self.buttonyloc + self.buttonwidth > mouse[1] > self.buttonyloc):
+                pygame.draw.rect(self.screen, LIGHTRED, (self.button1xloc, self.buttonyloc,self.buttonlength,self.buttonwidth))
+                if (click[0] == 1):
+                    return(False)
+            else:
+                pygame.draw.rect(self.screen, RED, (self.button1xloc, self.buttonyloc,self.buttonlength,self.buttonwidth))
+            smallfont=pygame.font.SysFont(self.fonttype, 20)
+            textSurf, textRect = self.text_objects(button1label, smallfont, fontcolortxt)
+            textRect.center = ( ((self.button1xloc)+(self.buttonlength/2))) , (self.buttonyloc+(self.buttonwidth/2))
+            self.screen.blit(textSurf, textRect)
+
+            if (self.button2xloc + self.buttonlength > mouse[0] > self.button2xloc and self.buttonyloc + self.buttonwidth > mouse[1] > self.buttonyloc):
+                pygame.draw.rect(self.screen, LIGHTRED, (self.button2xloc, self.buttonyloc,self.buttonlength,self.buttonwidth))
+                if (click[0] == 1):
+                    return(True)
+            else:
+                pygame.draw.rect(self.screen, RED, (self.button2xloc, self.buttonyloc,self.buttonlength,self.buttonwidth))
+            textSurf, textRect = self.text_objects(button2label, smallfont, fontcolortxt)
+            textRect.center = ( ((self.button2xloc+(self.buttonlength/2))) , (self.buttonyloc+(self.buttonwidth/2)))
+            self.screen.blit(textSurf, textRect)
+        return(False)
--- four.py
+++ four.py
@@ -1,43 +1,46 @@
-
-
 import pygame
 import classes
 
-# Define some colors
-
 pygame.init()
 
-# Set the width and height of the screen [width, height]
-size = (600, 700)
+size = (700, 700)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Connect Four")
 
-# Loop until the user clicks the close button.
-# Used to manage how fast the screen updates
-#clock = pygame.time.Clock()
-
-# -------- Main Program Loop -----------
-MyGrid = classes.Grid(screen)
-done = False
-MyGrid.drawgrid()
-pygame.display.flip()
-
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                MyGrid.MoveMovingPiece('L')
-                pygame.display.flip()
-            if event.key == pygame.K_RIGHT:
-                MyGrid.MoveMovingPiece('R')
-                pygame.display.flip()
-            if event.key == pygame.K_DOWN:
-                MyGrid.MoveMovingPiece('D')
-                pygame.display.flip()
+BLACK=(0,0,0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+LIGHTRED = (100,0,0)
+YELLOW = (255,255,0)
+end_it=False
 
 
-pygame.quit()
+def main():
+    x = classes.screens("Times New Roman", screen)
+    x.screenmaking("Welcome to Connect Four!!", "Click the button to continue", BLUE, RED, BLACK, "Continue!","nothing", 'no')
+    x.screenmaking("Choose 1 or 2 Players", "Press the down arrow to continue", BLUE, RED, BLACK,"One Player", "Two Player", 'yes')
+
+    MyGrid = classes.Grid(screen)
+    done = False
+    MyGrid.drawgrid()
+    pygame.display.flip()
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    MyGrid.MoveMovingPiece('L')
+                    pygame.display.flip()
+                if event.key == pygame.K_RIGHT:
+                    MyGrid.MoveMovingPiece('R')
+                    pygame.display.flip()
+                if event.key == pygame.K_DOWN:
+                    MyGrid.MoveMovingPiece('D')
+                    pygame.display.flip()
+
+    pygame.quit()
+
+main()
